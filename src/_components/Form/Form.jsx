5d57b1b3ddfca3b19e56f5f5839ff71bf4fda958
Illya065 <illyakalyna.2000@gmail.com>
2021-03-04 20:16:54 +0200

    create header

---
+++ src/_components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <form className="form">
+      <input type="text" />
+      <button>
+        <div className="btn__img"></div>
+      </button>
+    </form>
+  );
+};
---
+++ src/_components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__content">
+          <p>tracker</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
